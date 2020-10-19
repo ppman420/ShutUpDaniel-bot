285f41ab0945ec1d879f18bd524f6309233e6075
ppman420 <68097633+ppman420@users.noreply.github.com>
2020-10-19 08:48:34 -0400

    Make it so saying the person's name first also works

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const {token, name} = require('./config.json');
 const client = new Discord.Client();
 const TheHolyBible = `shut up ${name}`
+const MrLsHentaiCollection = `${name} shut up`
 
 client.once('ready', () => {
 	console.log(`Ready to tell ${name} to shut up`);
@@ -9,7 +10,7 @@ client.once('ready', () => {
 
 client.on('message', message => {
 if (message.author.bot) return;
-	if (message.content.includes (TheHolyBible)) {
+	if (message.content.includes (TheHolyBible) || message.content.includes (MrLsHentaiCollection)) {
 		console.log(`${message.author.tag} (${message.author.id}) says shut up ${name}!`)
 		message.channel.send(`shut up ${name}`)
 	}
