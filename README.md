a407088a79cd1f6d72791847d1df91791fc7de78
ppman420 <68097633+ppman420@users.noreply.github.com>
2020-10-19 20:48:37 -0400

    Major improvements

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ A discord bot that says shut up to a person. Rename `config.json.template` to `c
 
 Linux:
   Download the source code, cd into it, run `npm bot.js`
+
+# How to configure
+
+Config.js:
+	token:      	your bot's token
+	name:       	the person's name
+	id          	the person's id
+	messages    	how many messages the person needs to say before the bot automagically tells then to shut up
+	auto			true/false, controls if the bot should tell the person to shut up automagically
+	repeat			true/false, controls if the bot should tell the person to shut up after someone else says it
--- bot.js
+++ bot.js
@@ -1,19 +1,57 @@
+// Get discord.js
+const { Console } = require('console');
 const Discord = require('discord.js');
-const {token, name} = require('./config.json');
+// Get things from config.json
+const {token, name, id, messages, auto, repeat} = require('./config.json');
 const client = new Discord.Client();
+// Spaghetti code
 const TheHolyBible = `shut up ${name}`
 const MrLsHentaiCollection = `${name} shut up`
+var i = 0
 
 client.once('ready', () => {
-	console.log(`Ready to tell ${name} to shut up`);
+    console.log(`Ready!`);
 });
 
 client.on('message', message => {
-if (message.author.bot) return;
-	if (message.content.includes (TheHolyBible) || message.content.includes (MrLsHentaiCollection)) {
-		console.log(`${message.author.tag} (${message.author.id}) says shut up ${name}!`)
-		message.channel.send(`shut up ${name}`)
-	}
+    // Spaghetti code from this point onwards
+    // if else if else if else if else...
+    // Make bot not responde to self
+    if (message.author.bot) return;
+    if (message.content.includes (TheHolyBible) || message.content.includes (MrLsHentaiCollection)) {
+        // if repeat is false, don't execute the code
+        if (repeat == true) {
+            // Checks if the author is ${name}
+            if (message.author.id != id) {
+                console.log(`${message.author.tag} (${message.author.id} says shut up ${name}).`)
+                message.channel.send(`shut up ${name}`)
+            }
+            // If ${name} tells ${name} to shut up, be more rude
+            else if (message.author.id == id) {
+                console.log(`${name} says shut up ${name}`)
+                message.channel.send(`You are right, why don't you shut up ${name}?`)
+                return;
+            }
+        }
+    }
+    if (message.author.id == id) {
+        // if auto is false, don't execute the code
+        if (auto == true) {
+            // Wait and subtract 1 from i if i is greater than 0
+            if (i > 0) {
+                i--
+                console.log(`Waiting for ${name} to send ${i} messages before saying shut up ${name}.`)
+                return;
+            }
+            // Tell ${name} to shut up if i is 0
+            else {
+                console.log(`Telling ${name} to shut up.`)
+                message.channel.send(`shut up ${name}`)
+                i = messages
+                return;
+            }
+        }
+    }
 });
 
 client.login(token);
