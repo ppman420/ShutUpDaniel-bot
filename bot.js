41a2dffa97f5b524d3b11faa87a530fe6df1cea1
ppman420 <68097633+ppman420@users.noreply.github.com>
2020-10-23 13:14:19 -0400

    Add repeat speed up feature

--- bot.js
+++ bot.js
@@ -2,20 +2,23 @@
 const { Console } = require('console');
 const Discord = require('discord.js');
 // Get things from config.json
-const {token, name, id, auto, messages, repeat} = require('./config.json');
+const {token, name, id, auto, messages, repeat, repeatSpeedUpAutoAmount} = require('./config.json');
 const client = new Discord.Client();
 // Spaghetti code
 const TheHolyBible = `shut up ${name}`
 const MrLsHentaiCollection = `${name} shut up`
-var i = 0
+var i = messages
+var e = repeatSpeedUpAutoAmount
 
 client.once('ready', () => {
+    // Say all this shit when starting
     console.log(`Ready!`);
-    console.log(`Name is ${name}!`)
-    console.log(`ID is ${id}!`)
-    console.log(`Auto is ${auto}!`)
-    console.log(`Auto Message count is ${messages}!`)
-    console.log(`Repeat is ${repeat}!`)
+    console.log(`Name is ${name}.`)
+    console.log(`ID is ${id}.`)
+    console.log(`Auto is ${auto}.`)
+    console.log(`Auto Message count is ${messages}.`)
+    console.log(`Repeat is ${repeat}.`)
+    console.log(`repeatSpeedUpAutoAmount is ${repeatSpeedUpAutoAmount}`)
 });
 
 client.on('message', message => {
@@ -24,6 +27,9 @@ client.on('message', message => {
     // Make bot not responde to self
     if (message.author.bot) return;
     if (message.content.includes (TheHolyBible) || message.content.includes (MrLsHentaiCollection)) {
+        // Minus i by repeatSpeedUpAutoAmount
+        i = (i - repeatSpeedUpAutoAmount)
+        console.log(`${message.author.tag} (${message.author.id}) said shut up ${name}, waiting for ${i} messages before auto`)
         // if repeat is false, don't execute the code
         if (repeat == true) {
             // Checks if the author is ${name}
@@ -45,12 +51,12 @@ client.on('message', message => {
             // Wait and subtract 1 from i if i is greater than 0
             if (i > 0) {
                 i--
-                console.log(`Waiting for ${name} to send ${i} messages before saying shut up ${name}.`)
+                console.log(`Waiting for ${name} to send ${i} messages before auto`)
                 return;
             }
             // Tell ${name} to shut up if i is 0
             else {
-                console.log(`Telling ${name} to shut up.`)
+                console.log(`Telling ${name} to shut up`)
                 message.channel.send(`shut up ${name}`)
                 i = messages
                 return;
